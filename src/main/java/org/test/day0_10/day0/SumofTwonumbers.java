71a42a9c9cd2252894c22ec901d5739745730b5b
Jaehwan-sin <fate427@naver.com>
2024-05-06 01:11:12 +0900

    day0

--- src/main/java/org/test/day0_10/day0/SumofTwonumbers.java
+++ src/main/java/org/test/day0_10/day0/SumofTwonumbers.java
@@ -1,10 +1,26 @@
-package org.test.day0_10;
+package org.test.day0_10.day0;
 
-/**
- *  문제 : 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
- */
 public class SumofTwonumbers {
+    // 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
     public int solution(int num1, int num2) {
         return num1+num2;
-    }  
+    }
+
+    // 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+    public int solution2(int num1, int num2) {
+        return num2-num1;
+    }
+
+    // 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+    public int solution3(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+
+    // 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+    public int solution4(int num1, int num2) {
+        return num1 / num2;
+    }
+
 }
+
--- src/test/java/org/test/day0_10/SumofTwonumbersTest.java
+++ src/test/java/org/test/day0_10/SumofTwonumbersTest.java
@@ -4,12 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.test.day0_10.day0.SumofTwonumbers;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SumofTwonumbersTest { 
+public class SumofTwonumbersTest {
 
     public SumofTwonumbersTest() {}
 
