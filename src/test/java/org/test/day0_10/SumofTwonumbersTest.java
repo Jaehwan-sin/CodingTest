84662125bfac682f441f5a46482c424b1d9509b9
Jaehwan-sin <fate427@naver.com>
2024-05-06 01:05:19 +0900

    day0

--- src/test/java/org/test/day0_10/SumofTwonumbersTest.java
+++ src/test/java/org/test/day0_10/SumofTwonumbersTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SumofTwonumbersTest {
+public class SumofTwonumbersTest { 
 
     public SumofTwonumbersTest() {}
 
