29d5677f71d2131fc029d7dad5eb20dca6455b1b
Jaehwan-sin <fate427@naver.com>
2024-05-07 23:37:26 +0900

    day0

--- src/test/java/org/test/day0_10/SumofTwonumbersDay0Test.java
+++ src/test/java/org/test/day0_10/SumofTwonumbersDay0Test.java
@@ -15,7 +15,7 @@ public class SumofTwonumbersDay0Test {
 
     @Test
     public void testSolution() {
-        SumofTwonumbers_day0 sumofTwonumbersDay0 = new SumofTwonumbers_day0();
+        day0 sumofTwonumbersDay0 = new day0();
         assertEquals(5, sumofTwonumbersDay0.solution(2, 3));
         assertEquals(10, sumofTwonumbersDay0.solution(5, 5));
         assertEquals(-1, sumofTwonumbersDay0.solution(-5, 4));
