7029d9f803c96e9e69de9700bde1474da9e4c6f3
Jaehwan-sin <fate427@naver.com>
2024-05-11 21:59:33 +0900

    day6

--- src/main/java/org/test/day0_10/day6.java
+++ src/main/java/org/test/day0_10/day6.java
@@ -13,9 +13,7 @@ public class day6 {
 
     // "*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다.
     // 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+    public String solution2() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
@@ -26,7 +24,20 @@ public class day6 {
             System.out.println();
         }
 
-        
+        return null;
     }
+    // 정수가 담긴 리스트 num_list가 주어질 때,
+    // num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+    public int[] solution3 (int[] num_list) {
+        int[] answer = new int[2];
 
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                answer[0]++;
+            } else {
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
 }
