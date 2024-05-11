c5558607a41d975c29d8351c1e2b6308dbdcd036
Jaehwan-sin <fate427@naver.com>
2024-05-11 22:21:55 +0900

    day6

--- src/main/java/org/test/day0_10/day6.java
+++ src/main/java/org/test/day0_10/day6.java
@@ -40,4 +40,12 @@ public class day6 {
         }
         return answer;
     }
+
+    // 문자열 my_string과 정수 n이 매개변수로 주어질 때,
+    // my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+    public String solution4 (String my_string, int n) {
+        String answer = "";
+
+        return answer;
+    }
 }
