c512478a2352e38ef7fd1d63b343ea3575aa1912
Jaehwan-sin <fate427@naver.com>
2024-05-12 21:24:07 +0900

    day7

--- src/main/java/org/test/day0_10/day7.java
+++ src/main/java/org/test/day0_10/day7.java
@@ -29,12 +29,25 @@ public class day7 {
         return answer;
     }
 
-//    머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다.
-//    양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때,
-//    양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
+    // 머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다.
+    // 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때,
+    // 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
     public int solution3 (int n, int k) {
         int answer = 0;
         answer = (12000 * n) + ((k-(n/10)) * 2000);
         return answer;
+    }
+
+    // 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+    public int solution4 (int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 }
