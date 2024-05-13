22536a4056df93c82af27ce7dd90457e006d12cf
Jaehwan-sin <fate427@naver.com>
2024-05-13 21:57:16 +0900

    day8

--- src/main/java/org/test/day0_10/day8.java
+++ src/main/java/org/test/day0_10/day8.java
@@ -1,53 +1,29 @@
 package org.test.day0_10;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
-public class day7 {
+public class day8 {
 
-    // 문자열 my_string과 문자 letter이 매개변수로 주어집니다.
-    // my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
-    public String solution(String my_string, String letter) {
-        return my_string.replace(letter, "");
+    // 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때,
+    // numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+    public int[] solution (int[] numbers, int num1, int num2) {
+        int[] answer = new int[numbers.length];
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 
-    // 각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다.
-    // 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
-    // 예각 : 0 < angle < 90 직각 : angle = 90 둔각 : 90 < angle < 180 평각 : angle = 180
-    public int solution(int angle) {
-        int answer = 0;
+    // 우주여행을 하던 머쓱이는 엔진 고장으로 PROGRAMMERS-962 행성에 불시착하게 됐습니다.
+    // 입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다.
+    // a는 0, b는 1, c는 2, ..., j는 9입니다. 예를 들어 23살은 cd, 51살은 fb로 표현합니다.
+    // 나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요.
+    public String solution2 (int age) {
+        String answer = "";
+        String[] Alpha = new String[]{"a","b","c","d","e","f","g","h","i","j"};
 
-        if (0 < angle && angle < 90) {
-            answer = 1;
-        } else if (angle == 90) {
-            answer = 2;
-        } else if (90 < angle && angle < 180) {
-            answer = 3;
-        } else if (angle == 180) {
-            answer = 4;
+        while(age>0) {
+            answer = Alpha[age % 10] + answer;
+            age /= 10;
         }
 
         return answer;
-    }
-
-    // 머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다.
-    // 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때,
-    // 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
-    public int solution3 (int n, int k) {
-        int answer = 0;
-        answer = (12000 * n) + ((k-(n/10)) * 2000);
-        return answer;
-    }
-
-    // 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
-    public int solution4 (int n) {
-        int sum = 0;
-
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
-        }
-
-        return sum;
     }
 }
