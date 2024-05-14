b0a8af31caabd666633e74f6e2c4fcc23a6f2e6b
Jaehwan-sin <fate427@naver.com>
2024-05-14 23:55:04 +0900

    day9

--- src/main/java/org/test/day0_10/day9.java
+++ src/main/java/org/test/day0_10/day9.java
@@ -73,5 +73,16 @@ public class day9 {
             return answer;
         }
 
-        √
+        // 머쓱이는 구슬을 친구들에게 나누어주려고 합니다.
+        // 구슬은 모두 다르게 생겼습니다.
+        // 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때,
+        // balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+        public int solution4 (int balls, int share) {
+            return combination(balls, share);
+        }
+
+        public static int combination (int balls, int share) {
+            if (balls == share || share == 0) return 1;
+            return combination((balls - 1), (share - 1)) + combination(balls - 1, share);
+        }
 }
