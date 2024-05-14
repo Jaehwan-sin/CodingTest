2b4773e82c94ba4762f044125b731f4b7f6cecb7
Jaehwan-sin <fate427@naver.com>
2024-05-14 21:39:05 +0900

    BAEKJOON

--- src/main/java/org/test/baekjoon/a_2588.java
+++ src/main/java/org/test/baekjoon/a_2588.java
@@ -1,2 +1,17 @@
-package org.test.baekjoon;public class a_2588 {
+package org.test.baekjoon;
+
+import java.util.Scanner;
+
+public class a_2588 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        String B = sc.next();
+
+        System.out.println(A* (B.charAt(2) - '0'));
+        System.out.println(A* (B.charAt(1) - '0'));
+        System.out.println(A* (B.charAt(0) - '0'));
+        System.out.println(A* Integer.parseInt(B));
+    }
 }
