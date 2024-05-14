de4474c1930ce170b1e68933975623c90bcf5f75
Jaehwan-sin <fate427@naver.com>
2024-05-15 01:00:24 +0900

    BAEKJOON

--- src/main/java/org/test/baekjoon/a_2588.java
+++ src/main/java/org/test/baekjoon/a_2588.java
@@ -3,6 +3,11 @@ package org.test.baekjoon;
 import java.util.Scanner;
 
 public class a_2588 {
+
+    /** 문제 : (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+        (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+     *  https://www.acmicpc.net/problem/2588
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
