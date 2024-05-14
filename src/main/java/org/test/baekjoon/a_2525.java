24ef1c5c7c5f850035d9815297f30fc275d37e9f
Jaehwan-sin <fate427@naver.com>
2024-05-15 00:58:37 +0900

    BAEKJOON

--- src/main/java/org/test/baekjoon/a_2525.java
+++ src/main/java/org/test/baekjoon/a_2525.java
@@ -1,2 +1,35 @@
-package org.test.baekjoon;public class a_2525 {
+package org.test.baekjoon;
+
+import java.util.Scanner;
+
+public class a_2525 {
+
+    /**
+     * 문제 : KOI 전자에서는 건강에 좋고 맛있는 훈제오리구이 요리를 간편하게 만드는 인공지능 오븐을 개발하려고 한다.
+     * 인공지능 오븐을 사용하는 방법은 적당한 양의 오리 훈제 재료를 인공지능 오븐에 넣으면 된다.
+     * 그러면 인공지능 오븐은 오븐구이가 끝나는 시간을 분 단위로 자동적으로 계산한다.
+     * 또한, KOI 전자의 인공지능 오븐 앞면에는 사용자에게 훈제오리구이 요리가 끝나는 시각을 알려 주는 디지털 시계가 있다.
+     * 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때, 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
+     *
+     * 입력 : 첫째 줄에는 현재 시각이 나온다. 현재 시각은 시 A (0 ≤ A ≤ 23) 와 분 B (0 ≤ B ≤ 59)가 정수로 빈칸을 사이에 두고 순서대로 주어진다.
+     * 두 번째 줄에는 요리하는 데 필요한 시간 C (0 ≤ C ≤ 1,000)가 분 단위로 주어진다.
+     */
+
+    public static void main(String args[])  {
+        Scanner sc = new Scanner(System.in);
+
+        int H = sc.nextInt(); // 시
+        int M = sc.nextInt(); // 분
+        int CookingTime = sc.nextInt(); // 요리하는데 필요한 시간
+        sc.close();
+
+        M = H * 60 + M;
+        M = M + CookingTime;
+
+        H = (M / 60) % 24; // 시 (24시 이상이면 0시부터 시작)
+        int min = M % 60; // 분
+
+        System.out.println(H + " " + min);
+
+    }
 }
