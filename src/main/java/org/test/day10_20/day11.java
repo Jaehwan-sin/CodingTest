1eb5a64a1a457c1ba775f2daf295ab6978557213
Jaehwan-sin <fate427@naver.com>
2024-05-17 01:28:00 +0900

    day11

--- src/main/java/org/test/day10_20/day11.java
+++ src/main/java/org/test/day10_20/day11.java
@@ -34,4 +34,24 @@ public class day11 {
         Arrays.sort(numbers);
         return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
+
+    // i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다.
+    // 예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다. 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
+    // i! ≤ n
+    public int solution4 (int n) {
+        int answer = 0;
+        for (int i = 1; i <= 10; i++) {
+            if (n >= factorial(i)) {
+                answer = i;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static int factorial(int number) {
+        if (number > 1) return number * factorial(number - 1);
+        return number;
+    }
 }
