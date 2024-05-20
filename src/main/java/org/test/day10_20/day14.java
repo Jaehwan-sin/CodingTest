aab728e35f718643a0b1ab276c4a6402b81f1bef
Jaehwan-sin <fate427@naver.com>
2024-05-20 15:28:35 +0900

    day14

--- src/main/java/org/test/day10_20/day14.java
+++ src/main/java/org/test/day10_20/day14.java
@@ -34,4 +34,22 @@ public class day14 {
 
         return answer;
     }
+
+    // 군 전략가 머쓱이는 전쟁 중 적군이 다음과 같은 암호 체계를 사용한다는 것을 알아냈습니다.
+    // 암호화된 문자열 cipher를 주고받습니다.
+    // 그 문자열에서 code의 배수 번째 글자만 진짜 암호입니다.
+    // 문자열 cipher와 정수 code가 매개변수로 주어질 때 해독된 암호 문자열을 return하도록 solution 함수를 완성해주세요.
+    public String solution3 (String cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = code-1; i < cipher.length(); i += code) {
+            sb.append(cipher.charAt(i));
+        }
+
+        return sb.toString();
+    }
+
+    // 문자열 my_string이 매개변수로 주어질 때,
+    // 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+
 }
