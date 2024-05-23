f00bd4bbf7127bfdbe1a51dfe97ee3f298b1d3eb
Jaehwan-sin <fate427@naver.com>
2024-05-24 00:51:52 +0900

    day18

--- src/main/java/org/test/day10_20/day18.java
+++ src/main/java/org/test/day10_20/day18.java
@@ -1,5 +1,7 @@
 package org.test.day10_20;
 
+import java.util.Arrays;
+
 public class day18 {
 
     // 문자열 str1, str2가 매개변수로 주어집니다.
@@ -30,4 +32,33 @@ public class day18 {
             return 2;
         }
     }
+
+    // 어떤 세균은 1시간에 두배만큼 증식한다고 합니다.
+    // 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+    public int solution3 (int n, int t) {
+        int answer = 0;
+
+        for (int i = 1; i <= t ; i++) {
+            answer = n *= 2;
+        }
+
+        return answer;
+    }
+
+    // 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때,
+    // my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+
+        char[] chArr = my_string.toCharArray();
+        Arrays.sort(chArr);
+
+        String answer = new String(chArr);
+        return answer;
+    }
+    //  my_string을 소문자로 변경한다. ( toLowerCase() )
+    //  변경한 문자열을 char 배열에 각각 넣는다. ( toCharArray() )
+    //  사전 순으로 정렬한다. ( Arrays.sort() )
+    //  정렬한 char 배열을 String 생성자의 인자로 넣어서 문자열로 변환시켜 answer에 저장한다.
+
 }
