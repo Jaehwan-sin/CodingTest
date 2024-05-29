c366903c3f0a78d381069aad34aa9e30628d6513
Jaehwan-sin <fate427@naver.com>
2024-05-29 23:03:17 +0900

    day23

--- src/main/java/org/test/day21_30/day23.java
+++ src/main/java/org/test/day21_30/day23.java
@@ -50,4 +50,41 @@ public class day23 {
 
         return answer;
     }
+
+    // 머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다.
+    // 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다.
+    // 문자열 배열 babbling이 매개변수로 주어질 때,
+    // 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] check= {"aya", "ye", "woo", "ma"};
+
+        for(String b : babbling) {                  //babbling 에 있는 String 을 하나씩 꺼내옴
+            for(String c : check) {                 //check 에 있는 String 을 하나씩 꺼내옴
+                b=b.replace(c, " ");                //b 에 c 가 있으면 일단 전부 공백으로 변경(나머지를 구분 하기위해)
+            }
+            if(b.replaceAll(" ", "").equals("")) {  //공백처리한 b 에 있는 공백을 없에고 남은게 아무것도 없으면
+                answer++;                           //answer에 카운트+1
+            }
+        }
+        return answer;
+    }
+
+    // 머쓱이는 프로그래머스에 로그인하려고 합니다.
+    // 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때,
+    // 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+    // 아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
+    // 로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
+    public String solution(String[] id_pw, String[][] db) {
+        for(int i = 0; i<db.length; i++) {
+            if(id_pw[0].equals(db[i][0])) {
+                if(id_pw[1].equals(db[i][1])) {
+                    return "login";
+                } else {
+                    return "wrong pw";
+                }
+            }
+        }
+        return "fail";
+    }
 }
