4976f74a178c885193c0bfa0509add90c436827d
Jaehwan-sin <fate427@naver.com>
2024-05-31 16:25:55 +0900

    day25

--- src/main/java/org/test/day21_30/day25.java
+++ src/main/java/org/test/day21_30/day25.java
@@ -58,4 +58,18 @@ public class day25 {
 
     // 등차수열 혹은 등비수열 common이 매개변수로 주어질 때,
     // 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+    public int solution4 (int[] common) {
+        int answer = 0;
+
+        int x = common[1] - common[0];
+        int y = common[2] - common[1];
+
+        if (x == y) {
+            answer = common[common.length - 1] + y;
+        } else {
+            answer = common[common.length - 1] * common[2] / common[1];
+        }
+
+        return answer;
+    }
 }
