a1f2a8b214a34e0cf9f18bfce036c10dea3caf02
Jaehwan-sin <fate427@naver.com>
2024-06-02 03:39:59 +0900

    day2

--- src/main/java/org/test/basic_day1_10/day2.java
+++ src/main/java/org/test/basic_day1_10/day2.java
@@ -42,5 +42,29 @@ public class day2 {
             System.out.println(a.substring(i, i + 1));
         }
     }
-    
+
+    // 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+    public static void main4 (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n % 2 == 0) {
+            System.out.print(n + " is even");
+        } else {
+            System.out.print(n + " is odd");
+        }
+    }
+
+    // 문자열 my_string, overwrite_string과 정수 s가 주어집니다.
+    // 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을
+    // 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s) + overwrite_string;
+
+        if(my_string.length() > answer.length()) {
+            answer += my_string.substring(answer.length());
+        }
+
+        return answer;
+    }
 }
