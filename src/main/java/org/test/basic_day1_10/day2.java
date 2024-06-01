648e7526d9b72e8fa4e703a22339129b3b400139
Jaehwan-sin <fate427@naver.com>
2024-06-02 03:27:33 +0900

    day2

---
+++ src/main/java/org/test/basic_day1_10/day2.java
@@ -0,0 +1,16 @@
+package org.test.basic_day1_10;
+
+import java.util.Scanner;
+
+public class day2 {
+
+    // 두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+    // a + b = c
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
