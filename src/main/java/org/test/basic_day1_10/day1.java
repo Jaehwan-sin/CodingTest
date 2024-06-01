ee3a7278561d5ff3ecf387dfe8a02ae9ced5d5c8
Jaehwan-sin <fate427@naver.com>
2024-06-01 14:39:36 +0900

    Basic Day1

---
+++ src/main/java/org/test/basic_day1_10/day1.java
@@ -0,0 +1,17 @@
+package org.test.basic_day1_10;
+
+import java.util.Scanner;
+
+public class day1 {
+
+    // 프로그래머스 기초 트레이닝 Day1~25 문제 풀이
+    // 문자열 str이 주어질 때, str을 출력하는 코드를 작성해보세요.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+
+    
+}
