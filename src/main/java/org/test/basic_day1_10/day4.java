80e7b3f2ae8ff610c6de7d424bb93b4728069d1b
Jaehwan-sin <fate427@naver.com>
2024-06-04 00:28:34 +0900

    Basic Day4

--- src/main/java/org/test/basic_day1_10/day4.java
+++ src/main/java/org/test/basic_day1_10/day4.java
@@ -6,7 +6,7 @@ public class day4 {
     // num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
     public int solution (int num, int n) {
         return num % n == 0 ? 1 : 0;
-    }
+    } 
 
     // 정수 number와 n, m이 주어집니다.
     // number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록 solution 함수를 완성해주세요.
