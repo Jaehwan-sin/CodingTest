9b51805893a760fa8af1b71c7b30523d5d283c36
Jaehwan-sin <fate427@naver.com>
2024-06-03 23:56:21 +0900

    day3

--- src/main/java/org/test/basic_day1_10/day3.java
+++ src/main/java/org/test/basic_day1_10/day3.java
@@ -34,7 +34,7 @@ public class day3 {
 
     // 문자열 my_string과 정수 k가 주어질 때,
     // my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
-    public String solution(String my_string, int k) {
+    public String solution3 (String my_string, int k) {
         String answer = "";
 
         for (int i = 1; i <= k ; i++) {
@@ -42,5 +42,38 @@ public class day3 {
         }
 
         return answer;
+    }
+
+    // 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+    // 12 ⊕ 3 = 123
+    // 3 ⊕ 12 = 312
+    // 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+    // 단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+    public int solution4 (int a, int b) {
+        String ab = String.valueOf(a) + String.valueOf(b);
+        String ba = String.valueOf(b) + String.valueOf(a);
+
+        if (Integer.parseInt(ab) >= Integer.parseInt(ba)) {
+            return Integer.parseInt(ab);
+        } else {
+            return Integer.parseInt(ba);
+        }
+
+    }
+
+    // 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+    // 12 ⊕ 3 = 123
+    // 3 ⊕ 12 = 312
+    // 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+    // 단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+    public int solution(int a, int b) {
+        String sValue = String.valueOf(a) + String.valueOf(b);
+        int c = 2 * a * b;
+
+        if (Integer.parseInt(sValue) > c) {
+            return Integer.parseInt(sValue);
+        } else {
+            return c;
+        }
     }
 }
