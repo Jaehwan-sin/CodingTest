9a32176d4cda8dd5a241fa7732c90ea6305a9da5
Jaehwan-sin <fate427@naver.com>
2024-06-03 23:55:14 +0900

    day3

--- src/main/java/org/test/basic_day1_10/day3.java
+++ src/main/java/org/test/basic_day1_10/day3.java
@@ -20,55 +20,25 @@ public class day3 {
         return answer;
     }
 
-    // 두 개의 문자열 str1, str2가 공백으로 구분되어 입력으로 주어집니다.
-    // 입출력 예와 같이 str1과 str2을 이어서 출력하는 코드를 작성해 보세요.
-    // 입력 : apple pen
-    // 출력 : applepen
-    public static void main2 (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
+    // 문자들이 담겨있는 배열 arr가 주어집니다.
+    // arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+    public String solution2 (String[] arr) {
+        String answer = "";
 
-        System.out.println(a+b);
-    }
-
-    // 문자열 str이 주어집니다.
-    // 문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
-    // 입력 abcde
-    // 출력 a
-    // b
-    // c
-    // d
-    // e
-    public static void main3 (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-
-        for (int i = 0; i < a.length(); i++) {
-            System.out.println(a.substring(i, i + 1));
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
         }
+
+        return answer;
     }
 
-    // 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
-    public static void main4 (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    // 문자열 my_string과 정수 k가 주어질 때,
+    // my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+    public String solution(String my_string, int k) {
+        String answer = "";
 
-        if(n % 2 == 0) {
-            System.out.print(n + " is even");
-        } else {
-            System.out.print(n + " is odd");
-        }
-    }
-
-    // 문자열 my_string, overwrite_string과 정수 s가 주어집니다.
-    // 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을
-    // 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
-    public String solution(String my_string, String overwrite_string, int s) {
-        String answer = my_string.substring(0, s) + overwrite_string;
-
-        if(my_string.length() > answer.length()) {
-            answer += my_string.substring(answer.length());
+        for (int i = 1; i <= k ; i++) {
+            answer += (my_string);
         }
 
         return answer;
