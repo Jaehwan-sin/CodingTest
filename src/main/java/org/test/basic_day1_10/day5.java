7448a39aaf9063da921181be1d4a274dfdff8eb5
Jaehwan-sin <fate427@naver.com>
2024-06-05 01:09:38 +0900

    Basic day5

--- src/main/java/org/test/basic_day1_10/day5.java
+++ src/main/java/org/test/basic_day1_10/day5.java
@@ -79,7 +79,7 @@ public class day5 {
 
     // 정수가 담긴 리스트 num_list가 주어질 때,
     // 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
-    public int solution(int[] num_list) {
+    public int solution4 (int[] num_list) {
         int answer = 0;
         int sum = 0;
         int mul = 1;
@@ -91,6 +91,26 @@ public class day5 {
         if(sum * sum > mul){
             answer = 1;
         }
+
+        return answer;
+    }
+
+    // 정수가 담긴 리스트 num_list가 주어집니다.
+    // num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해주세요.
+    public int solution5 (int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even += Integer.toString(num_list[i]);
+            } else {
+                odd += Integer.toString(num_list[i]);
+            }
+        }
+
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
 
         return answer;
     }
