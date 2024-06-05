335c25122947a7d7feba004bbaff41b01a6e5146
Jaehwan-sin <fate427@naver.com>
2024-06-06 01:46:46 +0900

    Basic Day6

--- src/main/java/org/test/basic_day1_10/day6.java
+++ src/main/java/org/test/basic_day1_10/day6.java
@@ -101,7 +101,17 @@ public class day6 {
     // 각 쿼리의 순서에 맞게 답을 저장한 배열을 반환하는 solution 함수를 완성해 주세요.
     // 단, 특정 쿼리의 답이 존재하지 않으면 -1을 저장합니다.
     public int[] solution5 (int[] arr, int[][] queries) {
-        int[] answer = {};
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, Integer.MAX_VALUE);
+
+        for (int j = 0; j < queries.length; j++) {
+            for (int i = queries[j][0]; i <= queries[j][1]; i++) {
+                if (arr[i] > queries[j][2]) {
+                    answer[j] = Math.min(answer[j], arr[i]);
+                }
+            }
+            if (answer[j] == Integer.MAX_VALUE) answer[j] = -1;
+        }
         return answer;
     }
 }
