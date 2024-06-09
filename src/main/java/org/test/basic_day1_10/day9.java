457fa1d9caec1ed731ae077a1b669dc7374f5a31
Jaehwan-sin <fate427@naver.com>
2024-06-09 14:58:07 +0900

    day9

--- src/main/java/org/test/basic_day1_10/day9.java
+++ src/main/java/org/test/basic_day1_10/day9.java
@@ -1,6 +1,7 @@
 package org.test.basic_day1_10;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class day9 {
 
@@ -38,7 +39,28 @@ public class day9 {
     public String solution(String my_string, int n) {
         return my_string.substring(my_string.length() - n);
     }
-    
+
+    // 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다.
+    // 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
+    // 문자열 my_string이 매개변수로 주어질 때,
+    // my_string의 모든 접미사를 사전순으로 정렬한 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+    public String[] solution(String my_string) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            result.add(my_string.substring(i));
+        }
+        Collections.sort(result);
+
+        System.out.println("result = " + result);
+
+        String[] answer = new String[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        return answer;
+    }
+
     // 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다.
     // 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
     // 문자열 my_string과 is_suffix가 주어질 때,
