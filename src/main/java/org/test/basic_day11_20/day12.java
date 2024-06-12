725a65a4d94c482ef5e6ce1ca80a01a232d68d76
Jaehwan-sin <fate427@naver.com>
2024-06-12 15:22:02 +0900

    day12

--- src/main/java/org/test/basic_day11_20/day12.java
+++ src/main/java/org/test/basic_day11_20/day12.java
@@ -62,4 +62,34 @@ public class day12 {
         return answer;
     }
 
+    // 정수 배열 arr와 2개의 구간이 담긴 배열 intervals가 주어집니다.
+    // intervals는 항상 [[a1, b1], [a2, b2]]의 꼴로 주어지며 각 구간은 닫힌 구간입니다.
+    // 닫힌 구간은 양 끝값과 그 사이의 값을 모두 포함하는 구간을 의미합니다.
+    // 이때 배열 arr의 첫 번째 구간에 해당하는 배열과 두 번째 구간에 해당하는 배열을
+    // 앞뒤로 붙여 새로운 배열을 만들어 return 하는 solution 함수를 완성해 주세요.
+    public int[] solution(int[] arr, int[][] intervals) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int a1 = intervals[0][0];
+        int b1 = intervals[0][1];
+        int a2 = intervals[1][0];
+        int b2 = intervals[1][1];
+
+        for (int i = a1; i <= b1; i++) {
+            list.add(arr[i]);
+        }
+
+        for (int i = a2; i <= b2 ; i++) {
+            list.add(arr[i]);
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+    
 }
