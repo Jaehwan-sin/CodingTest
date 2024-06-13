31b265422ec182b41cf39fb435094e4fa48a1004
Jaehwan-sin <fate427@naver.com>
2024-06-13 16:39:24 +0900

    Basic Day13

--- src/main/java/org/test/basic_day11_20/day13.java
+++ src/main/java/org/test/basic_day11_20/day13.java
@@ -72,5 +72,16 @@ public class day13 {
         return Arrays.copyOfRange(num_list, 0, n);
     }
 
-    
+    // 정수 리스트 num_list와 정수 n이 주어질 때,
+    // num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+    public int[] solution5 (int[] num_list, int n) {
+        int[] answer = new int[(num_list.length + n - 1) / n];
+        int index = 0;
+
+        for (int i = 0; i < num_list.length; i += n) {
+            answer[index++] = num_list[i];
+        }
+
+        return answer;
+    }
 }
