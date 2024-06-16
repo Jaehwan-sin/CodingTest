d64ef4d5ee82e9afa905950ebb5546354ab81b95
Jaehwan-sin <fate427@naver.com>
2024-06-17 00:23:27 +0900

    Basic Day17

--- src/main/java/org/test/basic_day11_20/day17.java
+++ src/main/java/org/test/basic_day11_20/day17.java
@@ -40,4 +40,16 @@ public class day17 {
 
         return resultArr;
     }
+
+    // 단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때,
+    // my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+    public String[] solution4 (String my_string) {
+        return my_string.split(" ");
+    }
+
+    // 단어가 공백 한 개 이상으로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때,
+    // my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+    public String[] solution5 (String my_string) {
+        return my_string.trim().split("\\s+");
+    }
 }
