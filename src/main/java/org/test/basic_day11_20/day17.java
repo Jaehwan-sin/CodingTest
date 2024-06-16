6f33c87fdaa04a5a806d9ab1486982da63d96f82
Jaehwan-sin <fate427@naver.com>
2024-06-17 00:20:52 +0900

    day17

--- src/main/java/org/test/basic_day11_20/day17.java
+++ src/main/java/org/test/basic_day11_20/day17.java
@@ -1,2 +1,11 @@
-package org.test.basic_day11_20;public class day17 {
+package org.test.basic_day11_20;
+
+public class day17 {
+
+    // 문자열 myString과 pat가 주어집니다.
+    // myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
+    public String solution(String myString, String pat) {
+        int i = myString.lastIndexOf(pat);
+        return myString.substring(0, i) + pat;
+    }
 }
