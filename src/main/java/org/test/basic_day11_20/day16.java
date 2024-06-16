9a87a0471a040a20adaa2d52ef43a30d527402ef
Jaehwan-sin <fate427@naver.com>
2024-06-16 14:37:51 +0900

    day16

--- src/main/java/org/test/basic_day11_20/day16.java
+++ src/main/java/org/test/basic_day11_20/day16.java
@@ -10,10 +10,26 @@ public class day16 {
 
     // 알파벳으로 이루어진 문자열 myString이 주어집니다.
     // 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
-    public String solution2(String myString) {
+    public String solution2 (String myString) {
         return myString.toLowerCase();
     }
 
-    
+    // 문자열 배열 strArr가 주어집니다.
+    // 모든 원소가 알파벳으로만 이루어져 있을 때,
+    // 배열에서 홀수번째 인덱스의 문자열은 모든 문자를 대문자로,
+    // 짝수번째 인덱스의 문자열은 모든 문자를 소문자로 바꿔서 반환하는 solution 함수를 완성해 주세요.
+    public String[] solution (String[] strArr) {
+        String[] answer = new String[strArr.length];
+
+        for(int i=0; i<strArr.length; i++){
+            if(i%2==0){
+                answer[i] = strArr[i].toLowerCase();
+            }else{
+                answer[i] = strArr[i].toUpperCase();
+            }
+        }
+
+        return answer;
+    }
 
 }
