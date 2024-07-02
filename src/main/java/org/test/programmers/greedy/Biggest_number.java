d6911eed52b818446fd008ba4db48e30486b3647
Jaehwan-sin <fate427@naver.com>
2024-07-02 21:37:09 +0900

    greedy - 큰 수 만들기

--- src/main/java/org/test/programmers/greedy/Biggest_number.java
+++ src/main/java/org/test/programmers/greedy/Biggest_number.java
@@ -8,10 +8,7 @@ public class Biggest_number {
     // 예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
     // 문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다.
     // number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
-    public static void main(String[] args) {
-        String number = "1924";
-        int k = 2;
-
+    public String solution(String number, int k) {
         StringBuilder result = new StringBuilder();
         int length = number.length() - k; // 최종 결과 문자열의 길이
 
