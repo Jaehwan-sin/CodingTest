ddf21859b94b79f6108d85cb72f01c5ff6eb2f9a
jae hwan shin <fate427@naver.com>
2024-07-28 22:56:10 +0900

    프로세스

--- src/main/java/org/test/programmers/stack_queue/프로세스.java
+++ src/main/java/org/test/programmers/stack_queue/프로세스.java
@@ -17,7 +17,7 @@ public class 프로세스 {
     // 몇 번째로 실행되는지 알고싶은 프로세스의 위치를 알려주는 location이 매개변수로 주어질 때, 해당 프로세스가 몇 번째로 실행되는지 return 하도록 solution 함수를 작성해주세요.
     class Solution {
         public int solution(int[] priorities, int location) {
-            // 우선순위 큐 선언(내림 차순)
+            // 우선순위 큐 선언(내림 차순) 
             PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
             int answer = 0;
 
